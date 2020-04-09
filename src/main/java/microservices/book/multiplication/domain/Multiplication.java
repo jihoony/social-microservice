9bb00a39bd3dad74edf716f7d0a9fc462ce43a0e
jihoon <jihoon_yang@ksmartech.com>
2020-04-09 10:46:28 +0900

    first commit

---
+++ src/main/java/microservices/book/multiplication/domain/Multiplication.java
@@ -0,0 +1,39 @@
+package microservices.book.multiplication.domain;
+
+/**
+ *
+ */
+public class Multiplication {
+    // 인수
+    private int factorA;
+    private int factorB;
+
+    private int result;
+
+    public Multiplication(int factorA, int factorB) {
+        this.factorA = factorA;
+        this.factorB = factorB;
+        this.result = factorA * factorB;
+    }
+
+    public int getFactorA() {
+        return factorA;
+    }
+
+    public int getFactorB() {
+        return factorB;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Multiplication{" +
+                "factorA=" + factorA +
+                ", factorB=" + factorB +
+                ", result=" + result +
+                '}';
+    }
+}
