557a1fa4e44007e000958c866ac4e875ee535c0e
jihoon <jihoon_yang@ksmartech.com>
2020-04-17 16:40:11 +0900

    check serverport

--- src/main/java/microservices/book/multiplication/controller/MultiplicationController.java
+++ src/main/java/microservices/book/multiplication/controller/MultiplicationController.java
@@ -1,8 +1,10 @@
 package microservices.book.multiplication.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import microservices.book.multiplication.domain.Multiplication;
 import microservices.book.multiplication.service.MultiplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,19 +12,23 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * 곱셈 애플리케이션의 REST API 를 구현한 클래스
  */
+@Slf4j
 @RestController
 @RequestMapping("/multiplications")
 final class MultiplicationController {
 
     private final MultiplicationService multiplicationService;
+    private final int serverPort;
 
     @Autowired
-    public MultiplicationController(final MultiplicationService multiplicationService) {
+    public MultiplicationController(final MultiplicationService multiplicationService, @Value("${server.port}") int serverPort) {
         this.multiplicationService = multiplicationService;
+        this.serverPort = serverPort;
     }
 
     @GetMapping("/random")
     Multiplication getRandomMultiplication(){
+        log.info("무작위 곱셈이 생성된 서버 @ {}", serverPort);
         return multiplicationService.createRandomMultiplication();
     }
 }
--- src/main/java/microservices/book/multiplication/controller/MultiplicationResultAttemptController.java
+++ src/main/java/microservices/book/multiplication/controller/MultiplicationResultAttemptController.java
@@ -1,8 +1,10 @@
 package microservices.book.multiplication.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import microservices.book.multiplication.domain.MultiplicationResultAttempt;
 import microservices.book.multiplication.service.MultiplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,14 +13,17 @@ import java.util.List;
 /**
  * 사용자가 POST 로 답안을 전송하도록 REST API 를 제공하는 클래스
  */
+@Slf4j
 @RestController
 @RequestMapping("/results")
 final class MultiplicationResultAttemptController {
     private final MultiplicationService multiplicationService;
+    private final int serverPort;
 
     @Autowired
-    MultiplicationResultAttemptController(final MultiplicationService multiplicationService) {
+    MultiplicationResultAttemptController(final MultiplicationService multiplicationService, @Value("${server.port}") int serverPort) {
         this.multiplicationService = multiplicationService;
+        this.serverPort = serverPort;
     }
 
     @PostMapping
@@ -45,6 +50,9 @@ final class MultiplicationResultAttemptController {
 
     @GetMapping("/{resultId}")
     ResponseEntity<MultiplicationResultAttempt> getResultById(final @PathVariable("resultId") Long resultId) {
+
+        log.info("조회 결과 {}를 가져온 서버 @ {}", resultId, serverPort);
+
         return ResponseEntity.ok(
             multiplicationService.getResultById(resultId)
         );
